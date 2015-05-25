a8dbf43f1ab836c9351bed87a06f41a5108a7706
Michael Becco <e4e6d4d5@gmail.com>
2015-05-25 09:55:59 -0700

    Link Test and Home Controller Test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,8 @@ class HomeControllerTest < ActionController::TestCase
   def test_index
     get :index
     assert_response :success
+    assert_template 'home/index'
+    assert_select "title", "Yosemite Bug"
   end
 
 end
--- test/helpers/home_helper_test.rb
+++ test/helpers/home_helper_test.rb
@@ -2,8 +2,4 @@ require "test_helper"
 
 class HomeHelperTest < ActionView::TestCase
 
-  def test_sanity
-    flunk "Need real tests"
-  end
-
 end
