eabd6acd484361248c5a38a12b69c4bf3d47f206
Michael Becco <e4e6d4d5@gmail.com>
2015-06-14 10:57:46 -0700

    Add Cafe Page Controller/View/Test Text and Images

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'tweet_feed/index'
-
+  get 'cafe/index'
   root 'home#index'
 end
---
+++ test/helpers/cafe_helper_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class CafeHelperTest < ActionView::TestCase
+
+  def test_sanity
+
+  end
+
+end
