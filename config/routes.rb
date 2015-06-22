d48c947ae14317a86cf20ee3283d4e7366c1f822
Michael Becco <e4e6d4d5@gmail.com>
2015-06-22 08:46:01 -0700

    Add Route For Lodging Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :menus
   devise_for :users
   get 'tweet_feed/index'
+  get 'lodgings/index'
+  get 'lodgings/main'
   get 'cafe/index'
   root 'home#index'
 end
